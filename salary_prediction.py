a3b216e6147faa12176c4d0903e1b5500c7166ef
harshi26-03 <harshithan263@gmail.com>
2026-06-29 08:19:17 -0700

    Add salary_prediction.py
    Added the Python implementation of the Employee Salary Prediction project using Linear Regression. The script trains a machine learning model, predicts salary based on years of experience, and visualizes the regression line using Matplotlib.

---
+++ salary_prediction.py
@@ -0,0 +1,33 @@
+# Employee Salary Prediction using Machine Learning 
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression 
+# Sample Dataset 
+data = { 
+'YearsExperience': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
+'Salary': [25000, 30000, 35000, 40000, 50000, 
+55000, 60000, 65000, 70000, 80000] 
+}
+df = pd.DataFrame(data) 
+# Input and Output 
+X = df[['YearsExperience']] 
+y = df['Salary'] 
+# Split dataset 
+X_train, X_test, y_train, y_test = train_test_split( 
+X, y, test_size=0.2, random_state=42) 
+# Train Model 
+model = LinearRegression() 
+model.fit(X_train, y_train) 
+# Predict Salary 
+experience = float(input("Enter Years of Experience: ")) 
+predicted_salary = model.predict([[experience]]) 
+print("\nPredicted Salary: ₹", round(predicted_salary[0], 2)) 
+# Graph 
+plt.scatter(X, y) 
+plt.plot(X, model.predict(X), color='red') 
+plt.title("Employee Salary Prediction") 
+plt.xlabel("Years of Experience") 
+plt.ylabel("Salary") 
+plt.show()
